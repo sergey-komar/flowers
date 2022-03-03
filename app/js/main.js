2a57710f7621a04911e59540febd9e2f8f3cc4d5
sergey-komar <kursev81@yandex.ru>
2022-03-03 19:38:09 +0300

    start home

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
-})
+    $('.popular__block-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev-left"></button>',
+        nextArrow:'<button type="button" class="slick-next-right"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+    });
+});
